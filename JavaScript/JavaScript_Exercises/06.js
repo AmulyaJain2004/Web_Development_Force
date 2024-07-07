d930fe76d99aa3618c84f6d339fd24bf0ede3911
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-07-07 18:56:56 +0530

    Create 06.js

---
+++ JavaScript/JavaScript_Exercises/06.js
@@ -0,0 +1,9 @@
+function checkEvenOrOdd(num) {
+    if (num%2 == 0){
+        return "Even";
+    }
+    return "Odd";
+}
+
+console.log(checkEvenOrOdd(121));  // Odd
+console.log(checkEvenOrOdd(120));  // Even
