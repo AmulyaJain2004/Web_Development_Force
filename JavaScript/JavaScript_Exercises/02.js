da0c13bc42f886bba6832c11a55b1e50a4208926
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-07-07 16:51:32 +0530

    Create 02.js

---
+++ JavaScript/JavaScript_Exercises/02.js
@@ -0,0 +1,32 @@
+// Print the numbers form 1 to 5 to the console using a loop.
+
+console.log("Using a --> for loop");
+
+for (let i = 1; i <= 5; i++){
+    console.log(i)
+}
+
+// for (let i = 1; i <= 10; i++){
+//     console.log(i)
+// }
+
+
+// for (let i = 1; i <= 10; i+=2){
+//     console.log(i);
+// }
+
+console.log("Using a --> while loop");
+
+let i = 1;
+while(i<=5){
+    console.log(i);
+    i++;
+}
+
+console.log("Using a --> do-while loop");
+
+let j = 1;
+do {
+    console.log(j);
+    j++;
+}while(j<=5)
