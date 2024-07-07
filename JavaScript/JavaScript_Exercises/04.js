cc37348a0e27cbf86cd7a5c6ead355dfeee77c4b
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-07-07 16:52:16 +0530

    Create 04.js

---
+++ JavaScript/JavaScript_Exercises/04.js
@@ -0,0 +1,13 @@
+function calculateAreaOfRectangle(width, height){
+    return width*height;
+}
+
+let area = calculateAreaOfRectangle(5, 10);
+console.log(area)
+
+let calculateAreaOfRectangle = (width, height) => {
+    return width*height;
+}
+
+let area = calculateAreaOfRectangle(5, 70);
+console.log("Area of Rectangle is: " + area)
