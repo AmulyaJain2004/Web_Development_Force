8638da3730452a5f9eed56752ed8a86eea9d81e1
AmulyaJain2004 <jainamulyawin@gmail.com>
2025-04-18 23:19:56 +0530

    added codes

--- TaskManagementSystem/frontend/src/components/TaskStatusTabs.jsx
+++ TaskManagementSystem/frontend/src/components/TaskStatusTabs.jsx
@@ -1,21 +1,37 @@
 import React from 'react'
 
 const TaskStatusTabs = ({tabs, activeTab, setActiveTab}) => {
-  return <div className=''>
-    <div className=''>
+  return <div className='my-2'>
+    <div className='flex'>
         {tabs.map((tab) => (
             <button
                 key={tab.label}
-                className={`relatice px-3 md:px-4 py-2 text-sm font-medium ${
+                className={`relative px-3 md:px-4 py-2 text-sm font-medium ${
                     activeTab === tab.label 
                     ? "text-primary" 
-                    : "text-gray-200/60 hover:text-gray-700"
+                    : "text-gray-500 hover:text-gray-700"
                 } cursor-pointer`}
                 onClick={() => setActiveTab(tab.label)}
-            ></button>    
-        }
-    </div>
+            >
+                <div className='flex items-center'>
+                    <span className='text-xs'>{tab.label}</span>
+                    <span
+                        className={`text-xs ml-2 px-2 py-0.5 rounded-full ${
+                            activeTab === tab.label 
+                            ? "bg-primary text-white" 
+                            : "bg-gray-200/70 text-gray-600"
+                        }`}                        
+                    >
+                        {tab.count}
+                    </span>
+                </div>
+                {activeTab === tab.label && (
+                    <div className='absolute bottom-0 left-0 w-full h-0.5 bg-primary'></div>    
+                )}
+            </button>    
+        ))}
+        </div>
   </div>
-}
+};
 
 export default TaskStatusTabs
--- TaskManagementSystem/frontend/src/pages/Admin/ManageTasks.jsx
+++ TaskManagementSystem/frontend/src/pages/Admin/ManageTasks.jsx
@@ -5,7 +5,8 @@ import axiosInstance from '../../utils/axiosInstance';
 import { API_PATHS } from '../../utils/apiPaths';
 import { LuFileSpreadsheet } from 'react-icons/lu';
 import { all } from 'axios';
-import TaskStatusTabs from '../../components/TaskStatusTabs'; 
+import TaskStatusTabs from '../../components/TaskStatusTabs';
+import TaskCard from '../../components/Cards/TaskCard'; 
 
 const ManageTasks = () => {
   const [allTasks, setAllTasks] = useState([]);
@@ -43,6 +44,7 @@ const ManageTasks = () => {
 
   // download task report
   const handleDownloadReport = async () => {
+    
   };
 
   useEffect(() => {
@@ -66,7 +68,7 @@ const ManageTasks = () => {
             </button>  
           </div>  
 
-          {allTasks?.length > 0 && (
+          {tabs?.[0]?.count > 0 && (
             <div className='flex items-center gap-3'>
               <TaskStatusTabs
                 tabs={tabs}
@@ -80,6 +82,28 @@ const ManageTasks = () => {
               </button>
             </div>
           )}
+        </div>
+
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-4 mt-4'>
+          {allTasks?.map((item, index)=>(
+            <TaskCard
+              key={item._id}
+              title={item.title}
+              description={item.description}
+              priority={item.priority}
+              status={item.status}
+              progress={item.progress}
+              createdAt={item.createdAt}
+              dueDate={item.dueDate}
+              assignedTo={item.assignedTo?.map((item) =>item.profileImageUrl)}
+              attachmentCount={item.attachments?.length || 0}
+              completedTodoCount={item.completedTodoCount || 0}
+              todoChecklist={item.todoChecklist || []}
+              onClick={() => {
+                handleClick(item);
+              }}
+            />
+          ))}
         </div> 
       </div>
     </DashboardLayout>
